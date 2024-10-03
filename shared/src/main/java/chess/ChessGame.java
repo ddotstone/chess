357477716d0caf12b2f819dfb0834f46c10ec77c
Todd Johnston <johnstontoddc@gmail.com>
2024-10-02 23:10:52 -0600

    Added Set board

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -441,7 +441,7 @@ public class ChessGame {
      * @param board the new board to use
      */
     public void setBoard(ChessBoard board) {
-        throw new RuntimeException("Not implemented");
+        board = new ChessBoard(board);
     }
 
     /**
