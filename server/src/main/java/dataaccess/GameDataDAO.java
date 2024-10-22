ae2f72d5dca9687461f7698c2187f378f9b213ed
Todd Johnston <johnstontoddc@gmail.com>
2024-10-21 21:42:02 -0600

    Added Game Data DAO interface

--- server/src/main/java/dataaccess/GameDataDAO.java
+++ server/src/main/java/dataaccess/GameDataDAO.java
@@ -1,4 +1,13 @@
 package dataaccess;
 
-public class GameDataDAO {
+import model.GameData;
+
+import javax.xml.crypto.Data;
+
+public interface GameDataDAO {
+    public GameData getGame(String gameID) throws DataAccessException;
+
+    public void CreateGame(GameData gameData) throws DataAccessException;
+
+    public void UpdateGame(GameData gameData) throws DataAccessException;
 }
