0fcbefce413d794e2ff3906480c68c9da3601e48
Todd Johnston <johnstontoddc@gmail.com>
2024-10-21 22:22:30 -0600

    Added getGame

--- server/src/main/java/dataaccess/GameDataDAO.java
+++ server/src/main/java/dataaccess/GameDataDAO.java
@@ -3,14 +3,42 @@ package dataaccess;
 import model.GameData;
 
 import javax.xml.crypto.Data;
+import java.util.ArrayList;
 import java.util.Collection;
 
 public interface GameDataDAO {
-    public GameData getGame(String gameID) throws DataAccessException;
+    public GameData getGame(int gameID) throws DataAccessException;
 
     public void CreateGame(GameData gameData) throws DataAccessException;
 
     public void UpdateGame(GameData gameData) throws DataAccessException;
 
     public Collection<GameData> listGames() throws DataAccessException;
+}
+
+public class MemoryGameDataDAO implements GameDataDAO {
+    Collection<GameData> gameDataCollection;
+
+    public MemoryGameDataDAO() {
+        gameDataCollection = new ArrayList<>();
+        return;
+    }
+
+    public GameData getGame(int gameID) throws DataAccessException {
+        for (GameData game : gameDataCollection) {
+            if (game.gameID() == gameID) {
+                return game;
+            }
+        }
+        return null;
+    }
+
+    public void CreateGame(GameData gameData) throws DataAccessException {
+    }
+
+    public void UpdateGame(GameData gameData) throws DataAccessException {
+    }
+
+    public Collection<GameData> listGames() throws DataAccessException {
+    }
 }
