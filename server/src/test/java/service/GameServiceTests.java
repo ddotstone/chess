06b0c96e311c38e087785c5675b28c2a18585e17
Todd Johnston <johnstontoddc@gmail.com>
2024-10-22 18:17:35 -0600

    Added game service tests

--- server/src/test/java/service/GameServiceTests.java
+++ server/src/test/java/service/GameServiceTests.java
@@ -1,5 +1,6 @@
 package service;
 
+import chess.ChessGame;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -26,4 +27,125 @@ public class GameServiceTests {
         databaseService.clear();
     }
 
+    @Test
+    public void testCreateGame() throws DataAccessException {
+
+        // Register User
+        RegisterRequest registerRequest = new RegisterRequest("username", "password", "email");
+        RegisterResponse registerResponse = userService.register(registerRequest);
+
+        // Create Game
+        CreateGameRequest createGameRequest = new CreateGameRequest("game");
+        CreateGameResponse createGameResponse = gameService.CreateGame(registerResponse.authToken(), createGameRequest);
+
+        // List Game
+        ListGameResponse listGameResponse = gameService.ListGames(registerResponse.authToken());
+
+        // Assert Game Name Correct
+        Assertions.assertEquals(listGameResponse.games().size(), 1);
+        Assertions.assertEquals(listGameResponse.games().iterator().next().gameName(), "game");
+    }
+
+    @Test
+    public void testBadAuthCreateGame() throws DataAccessException {
+
+        // Create Game
+        CreateGameRequest createGameRequest = new CreateGameRequest("game");
+        Assertions.assertThrows(UnauthorizedException.class, () -> {
+            CreateGameResponse createGameResponse = gameService.CreateGame("not real", createGameRequest);
+        });
+    }
+
+    public void testListGame() throws DataAccessException {
+
+        // Register User
+        RegisterRequest registerRequest = new RegisterRequest("username", "password", "email");
+        RegisterResponse registerResponse = userService.register(registerRequest);
+
+        // Create Game
+        CreateGameRequest createGameRequest = new CreateGameRequest("game");
+        CreateGameResponse createGameResponse = gameService.CreateGame(registerResponse.authToken(), createGameRequest);
+
+        // List Game
+        ListGameResponse listGameResponse = gameService.ListGames(registerResponse.authToken());
+
+        // Assert Game Name Correct
+        Assertions.assertEquals(listGameResponse.games().size(), 1);
+        Assertions.assertEquals(listGameResponse.games().iterator().next().gameName(), "game");
+    }
+
+    @Test
+    public void testBadAuthListGame() throws DataAccessException {
+
+        // List Game
+        Assertions.assertThrows(UnauthorizedException.class, () -> {
+            ListGameResponse listGameResponse = gameService.ListGames("not real");
+        });
+    }
+
+    public void testUpdateGame() throws DataAccessException {
+
+        // Register User
+        RegisterRequest registerRequest = new RegisterRequest("username", "password", "email");
+        RegisterResponse registerResponse = userService.register(registerRequest);
+
+        // Create Game
+        CreateGameRequest createGameRequest = new CreateGameRequest("game");
+        CreateGameResponse createGameResponse = gameService.CreateGame(registerResponse.authToken(), createGameRequest);
+
+        // List Game
+        ListGameResponse listGameResponse = gameService.ListGames(registerResponse.authToken());
+
+        // Assert Game Name Correct
+        Assertions.assertEquals(listGameResponse.games().size(), 1);
+        Assertions.assertEquals(listGameResponse.games().iterator().next().gameName(), "game");
+
+        // Join Game
+        JoinGameRequest joinGameRequest = new JoinGameRequest(1, ChessGame.TeamColor.BLACK);
+        gameService.JoinGame(registerResponse.authToken(), joinGameRequest);
+
+        // List Game
+        listGameResponse = gameService.ListGames(registerResponse.authToken());
+
+        // Assert Black Player Name Correct
+        Assertions.assertEquals(listGameResponse.games().size(), 1);
+        Assertions.assertEquals(listGameResponse.games().iterator().next().blackUsername(), "username");
+    }
+
+    @Test
+    public void testCannotJoinAlreadyJoinedSpot() throws DataAccessException {
+
+        // Register User
+        RegisterRequest registerRequest = new RegisterRequest("username", "password", "email");
+        RegisterResponse registerResponse = userService.register(registerRequest);
+
+        // Create Game
+        CreateGameRequest createGameRequest = new CreateGameRequest("game");
+        CreateGameResponse createGameResponse = gameService.CreateGame(registerResponse.authToken(), createGameRequest);
+
+        // List Game
+        ListGameResponse listGameResponse = gameService.ListGames(registerResponse.authToken());
+
+        // Assert Game Name Correct
+        Assertions.assertEquals(listGameResponse.games().size(), 1);
+        Assertions.assertEquals(listGameResponse.games().iterator().next().gameName(), "game");
+
+        // Join Game
+        JoinGameRequest joinGameRequest = new JoinGameRequest(1, ChessGame.TeamColor.BLACK);
+        gameService.JoinGame(registerResponse.authToken(), joinGameRequest);
+
+        // List Game
+        listGameResponse = gameService.ListGames(registerResponse.authToken());
+
+        // Assert Black Player Name Correct
+        Assertions.assertEquals(listGameResponse.games().size(), 1);
+        Assertions.assertEquals(listGameResponse.games().iterator().next().blackUsername(), "username");
+
+        // Attempt Rejoin Game
+        joinGameRequest = new JoinGameRequest(1, ChessGame.TeamColor.BLACK);
+        JoinGameRequest finalJoinGameRequest = joinGameRequest;
+        Assertions.assertThrows(AlreadyTakenException.class, () -> {
+            gameService.JoinGame(registerResponse.authToken(), finalJoinGameRequest);
+        });
+    }
 }
