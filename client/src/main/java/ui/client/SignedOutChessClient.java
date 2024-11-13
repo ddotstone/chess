5368c87693bedd076775e80a4a6025356bf8e16d
Todd Johnston <johnstontoddc@gmail.com>
2024-11-12 23:23:53 -0700

    Added in response exception and client

---
+++ client/src/main/java/ui/client/SignedOutChessClient.java
@@ -0,0 +1,29 @@
+package ui.client;
+
+import java.util.Arrays;
+
+import com.google.gson.Gson;
+import exception.ResponseException;
+import server.ServerFacade;
+
+public class SignedOutChessClient {
+    public String eval(String input) {
+        try {
+            var tokens = input.toLowerCase().split(" ");
+            var cmd = (tokens.length > 0) ? tokens[0] : "help";
+            var params = Arrays.copyOfRange(tokens, 1, tokens.length);
+            return switch (cmd) {
+                case "signin" -> signIn(params);
+                case "rescue" -> register(params);
+                case "quit" -> "quit";
+                default -> help();
+            };
+        } catch (ResponseException ex) {
+            return ex.getMessage();
+        }
+    }
+
+    private String signIn(String... params) throws ResponseException {
+
+    }
+}
